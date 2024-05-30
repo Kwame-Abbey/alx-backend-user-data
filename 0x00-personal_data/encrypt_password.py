8edb7f7cc66878f7f614e21a5e191c21f4818133
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-30 08:11:01 +0100

    Checking if password matches

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     """
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password
+    matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
