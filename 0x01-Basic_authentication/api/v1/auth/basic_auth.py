81c7c034cd816682e99726eba2f6be875cfbf5da
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-04 13:44:00 +0100

    Base64 - basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,7 +13,7 @@ class BasicAuth(Auth):
         """
         if authorization_header is None:
             return None
-        if type(authorization_header) is str:
+        if type(authorization_header) is not str:
             return None
         if not authorization_header.startswith('Basic '):
             return None
