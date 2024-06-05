3e55709419209d6b0953438fc3dbe30e677c0ee3
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-05 21:37:04 +0100

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -43,3 +43,14 @@ def session_authentication():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logouts from session"""
+    from api.v1.app import auth
+    session_destroyed = auth.destroy_session(request)
+    if not session_destroyed:
+        abort(404)
+    return jsonify({}), 200
