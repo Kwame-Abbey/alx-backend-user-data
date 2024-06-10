05ff8159d8186ae9da3f325c7131ecd8dc6c970e
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-10 13:47:51 +0100

    Registers new users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,14 +2,43 @@
 """Defines a hashed password
 method using brcpyt package"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
     """Takes in a string argument
     Args:
         password (str): Password of user
-    Return:
+    Returns:
         Salted hash of the input password in bytes
     """
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers new users
+        Args:
+            email (str): email of the user
+            password (str): password of the new user
+        Returns:
+            The User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_password = _hash_password(password).decode('utf-8')
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
