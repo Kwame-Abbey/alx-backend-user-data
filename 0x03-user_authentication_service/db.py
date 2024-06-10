126448492ec4c56f567270ccf22a030771b44806
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-10 13:03:25 +0100

    Create User model and query using variable keyword arguments

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the database.
+
+        Args:
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The created User object.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter users.
+
+        Returns:
+            User: The first user found.
+
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If the query is invalid.
+        """
+        session = self._session
+        user = session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound("No user found")
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update the given user
+        Args:
+            user_id (int): The id of the user to update
+            **kwargs: The fields to update
+        Returns:
+            None
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise InvalidRequestError
+            self._session.commit()
+        except (NoResultFound, InvalidRequestError, ValueError):
+            raise ValueError
