4f954e31cd8b9f445061af4b555583e7a4661ce5
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-06-12 11:42:34 +0100

    Simplify code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -110,8 +110,7 @@ class Auth:
         if user_id is None:
             return None
         try:
-            user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user.id, session_id=None)
+            self._db.update_user(user_id=user_id, session_id=None)
             return None
         except (NoResultFound, ValueError):
             return None
